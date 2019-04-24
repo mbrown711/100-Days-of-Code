e2aead675e25747165c5213fc29ca40e21896404
Matt Brown <mbrown71189@gmail.com>
2019-04-24 16:50:16 -0400

    First commit of 100 Days of Code

---
+++ LearnPython3TheHardWay/ex2.py
@@ -0,0 +1,9 @@
+# A comment, this is so you can read your program later
+# Anything after the # is ignored in Python
+
+print("I could have left the code like this.") # and after the comment is ignored
+
+# You can also use a comment to "disable" or comment out code:
+# printf("This won't run")
+
+print("This will run")
