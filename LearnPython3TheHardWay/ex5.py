7a7c32efc385792ce8191fbe3c14491226e773e4
Matt Brown <mbrown71189@gmail.com>
2019-04-24 17:22:06 -0400

    Finished 3 more exercises before class

---
+++ LearnPython3TheHardWay/ex5.py
@@ -0,0 +1,17 @@
+myName = 'Matt Brown'
+myAge = 35
+myHeight = 6
+myWeight = 185
+myEyes = 'Blue'
+myTeeth = 'While'
+myHair = 'Brown'
+
+print(f"Let's talk about {myName}")
+print(f"He's {myHeight} feet tall")
+print(f"He's {myWeight} pounds heavy")
+print("Actually, that's not too heavy")
+print(f"He's got {myEyes} eyes and {myHair} hair")
+print(f"His teeth are usually {myTeeth} depending on the coffee")
+
+total = myAge + myHeight + myWeight
+print(f"If I add {myAge}, {myHeight}, and {myWeight} I get {total}")
