89375b9f36e64e5e14088820f806fd8675dcb5ee
Matt Brown <mbrown71189@gmail.com>
2019-04-25 22:20:53 -0400

    last commit of the day

--- PythonCrashCourse/motorcycles.py
+++ PythonCrashCourse/motorcycles.py
@@ -1,1 +1,25 @@
 motorcycles = ['honda', 'harley', 'yamaha', 'suzuki']
+print(motorcycles)
+
+# below replaces whatever index spot with a different value
+motorcycles[0] = 'ducati'
+
+print(motorcycles)
+
+#to add an element to a list, use .append()
+motorcycles.append('kawasaki')
+
+print(motorcycles)
+
+#could also add elements to a list by appending them to an empty list:
+new_motorcycles = []
+
+new_motorcycles.append('new bike 1')
+new_motorcycles.append('new bike 2')
+new_motorcycles.append('new bike 3')
+
+print(new_motorcycles)
+
+#can insert items into a list with .insert()
+motorcycles.insert(0, 'BMW')
+print("Inserting BMW into the motorcycles list: ", motorcycles)
