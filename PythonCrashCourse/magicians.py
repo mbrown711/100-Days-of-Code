285f42442ec1610eeafa17e9f44d2ab37a37198f
Matt Brown <mbrown71189@gmail.com>
2019-04-28 12:17:21 -0400

    starting chapter 4 with loops

---
+++ PythonCrashCourse/magicians.py
@@ -0,0 +1,5 @@
+magicians = ['alice', 'bob', 'matt', 'dalila', 'bailey']
+for magician in magicians: # use a new variable, store it, then print it
+    print(magician)
+
+#just a quick note - I can't believe how different this is from an array in Java or C
