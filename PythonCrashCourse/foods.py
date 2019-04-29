f9662fc68b88960864ff0deaf1fbee4ac1a7f712
Matt Brown <mbrown71189@gmail.com>
2019-04-29 17:30:29 -0400

    copying lists and tuples

---
+++ PythonCrashCourse/foods.py
@@ -0,0 +1,8 @@
+# copying a list - make a slice using the entire list you want to copy
+my_foods = ['pizza', 'buffalo wings', 'oreos', "resse's"]
+friend_foods = my_foods[:]
+
+print("My favorite foods are:")
+print(my_foods)
+print("My friend's favorite foods are:")
+print(friend_foods)
