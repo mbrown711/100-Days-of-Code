d8da8fa34617ee10a7f4efc121e2de4d5d23465b
Matt Brown <mbrown71189@gmail.com>
2019-05-01 20:52:18 -0400

    continuing if statements with else if

--- PythonCrashCourse/Chapter5_If_Statements/simpleifstatements.py
+++ PythonCrashCourse/Chapter5_If_Statements/simpleifstatements.py
@@ -16,3 +16,27 @@ if requested_toppings != 'anchovies':
 answer = 17
 if answer != 42:
     print("Wrong answer, try again!")
+
+# checking if a value is in a list:
+toppings = ['pepperoni', 'cheese', 'onions']
+is_mushrooms = 'mushrooms' in toppings # boolean here, fals because mushrooms is not in toppings
+print(is_mushrooms) # will print false
+
+banned_users = ['matt', 'bailey', 'dalila', 'mom', 'dad']
+user = 'chris'
+
+if user not in banned_users:
+    print(user.title() + ", you can post a response if you wish")
+
+age = 19
+if age >= 18:
+    print("Old enough to vote")
+    print("Have you registered to vote?")
+# think of indents as a code block in java
+
+age = 17
+if age >= 18:
+    print("Old enough to vote")
+    print("Have you registered to vote?")
+else:
+    print("Sorry, not old enough to vote yet")
