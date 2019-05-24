1f442294447c7c6cbe859ef437d40e95311322ea
Matt Brown <mbrown71189@gmail.com>
2019-05-24 17:52:26 -0400

    1 more if statement after this push

--- PythonCrashCourse/Chapter5_If_Statements/statements.py
+++ PythonCrashCourse/Chapter5_If_Statements/statements.py
@@ -13,3 +13,12 @@ elif age < 18:
     print("Your admission cost is $5")
 else:
     print("Your admission cost is $10")
+
+age = 13 
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 5
+else:
+    price = 10
+print("Your cost for admission is $" + str(price) + ".")
