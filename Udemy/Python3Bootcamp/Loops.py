ab65c887fc81fbe48b2f8a732fb55ee73bf4a80d
Matt Brown <mbrown71189@gmail.com>
2019-05-26 10:13:24 -0400

    exercises and while loops

--- Udemy/Python3Bootcamp/Loops.py
+++ Udemy/Python3Bootcamp/Loops.py
@@ -29,3 +29,35 @@ x = 0
 for i in range(11, 21, 2):
     x += i
 print(f"The sum using range is {x}")
+
+#repeating question exercise
+askingQuestions = input ("How many times do you want to repeat? ")
+askingQuestions = int(askingQuestions)
+for askingQuestion in range(askingQuestions):
+    print(f"Ok, repeating question. Repeat number: {askingQuestion + 1}")
+
+# loop through numbers 1 - 20 (inclusive)
+# for 4 and 13, print "x is unlucky"
+# for odd numbers, print "x is odd"
+
+for num in range (1, 21):
+    if num == 4 or num == 13:
+        state = "unlucky"
+    elif num % 2 == 0:
+        state = "even"
+    else:
+        state = "odd"
+    print(f"{num} is {state}")
+
+# while loops:
+
+msg = input("What's the secret password? ")
+while msg != "bananas":
+    print("Wrong!")
+    msg = input("What's the secret password? ")
+print("Correct!")
+
+number = 1
+while number <= 10:
+    print(number)
+    number += 1
