abbd6ed99f218f75ce3a1b7fb04377c6eaea428f
Matt Brown <home@Matts-MBP.fios-router.home>
2019-06-20 10:42:07 -0400

    Finished chapter 7

--- PythonCrashCourse/UserInputsWhileLoops.py
+++ PythonCrashCourse/UserInputsWhileLoops.py
@@ -50,3 +50,88 @@ while message != "quit":
     message = input(newPrompt)
     if message != "quit":
         print(message)
+
+# Using a flag
+print("\nUsing a flag below: ")
+active = True
+while active:
+    message = input(newPrompt)
+    if message == "quit":
+        active = False
+    else:
+        print(message)
+
+# Using break to exit a for loop:
+print("\nUsing break to exit a for loop: ")
+cityPrompt = "\nPlease enter the name of a city you have visited "
+cityPrompt += "\n(Enter 'quit' when you are finished): "
+
+while True:
+    city = input(cityPrompt)
+    if city == "quit":
+        break
+    else:
+        print("I'd like to go to " + city.title() + "!")
+
+# Using continue in a loop:
+print("\nUsing continue in a loop: ")
+currentNumber = 0
+while currentNumber <= 10:
+    currentNumber += 1
+    if currentNumber % 2 == 0:
+        continue
+
+    print(currentNumber)
+
+# Using a while loop with lists and dictionaries:
+
+print("\nMoving items from one list to another: ")
+# Start with a list of users that need to be verified, and an empty list to hold the users:
+unconfirmedUsers = ['alice', 'bob', 'brian']
+confirmedUsers = []
+
+# Verify each user until there are no more unconfirmed users
+# Move each unverified user into the list of confirmed users
+
+while unconfirmedUsers:
+    currentUser = unconfirmedUsers.pop()
+
+    print("Verifying user: " + currentUser.title())
+    confirmedUsers.append(currentUser)
+# Display all confirmed users:
+print("\nThe following users have been confirmed: ")
+for confirmedUser in confirmedUsers:
+    print(confirmedUser.title())
+
+# Removing all instances of specific values from a list:
+print("\nRemoving all instances of specific values from a list: ")
+pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
+print(pets)
+while "cat" in pets:
+    pets.remove("cat")
+print(pets)
+
+# Filing a dictionary with user input:
+print("\nFiling a dictionary with user input: ")
+
+responses = {}
+# Set a flag to indicate that polling is active
+pollingActive = True
+
+while pollingActive:
+    # Prompt for a person's name and response:
+    name = input("\nWhat is your name? ")
+    response = input("\nWhich mountain would you like to climb one day? ")
+    
+    # Store the responses in a dictionary:
+    responses[name] = response
+
+    # Find out if anyone else is going to take the poll:
+    repeat = input("\nWould you like to let someone else take the poll? (yes/no)? ")
+    if repeat == "no":
+        pollingActive = False
+
+# Polling is complete. Show the results:
+print("\n--- Poll Results ---")
+for name, response in responses.items():
+    print(name + " would like to climb " + response + ".")
